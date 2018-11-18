9c6da2925697e6a7412e92a647105f93fa0525bd
Brett Hartman <brett.hartman@mac.com>
2018-11-18 09:08:19 -0800

    Complete refactor to place server in separate module

--- index.js
+++ index.js
@@ -1,77 +1,13 @@
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const fs = require('fs');
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-const config = require('./config');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+const app = {};
 
-const router = {
-	ping: handlers.ping,
-	users: handlers.users,
-	tokens: handlers.tokens,
-	checks: handlers.checks
+app.init = () => {
+	server.init();
+	workers.init();
 };
 
-const unifiedServer = (req, res) => {
-	const parsedUrl = url.parse(req.url, true);
-	const path = parsedUrl.pathname;
-	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-	const queryStringObject = parsedUrl.query;
-	const method = req.method.toUpperCase();
-	const headers = req.headers;
-	const decoder = new StringDecoder('utf-8');
+app.init();
 
-	let buffer = '';
-
-	req.on('data', data => {
-		buffer += decoder.write(data);
-	});
-
-	req.on('end', () => {
-		buffer += decoder.end();
-
-		const chosenHandler = typeof router[trimmedPath] !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-		const data = {
-			trimmedPath,
-			queryStringObject,
-			method,
-			headers,
-			payload: helpers.parseJsonToObject(buffer)
-		};
-
-		chosenHandler(data, (statusCode, payload) => {
-			statusCode = typeof statusCode === 'number' ? statusCode : 200;
-			payload = typeof payload === 'object' ? payload : {};
-
-			const payloadString = JSON.stringify(payload);
-
-			res.setHeader('Content-Type', 'application/json');
-			res.writeHead(statusCode);
-			res.end(payloadString);
-		});
-	});
-};
-
-const httpServer = http.createServer((req, res) => {
-	unifiedServer(req, res);
-});
-
-httpServer.listen(config.httpPort, () => {
-	console.log(`HTTP server listening on port ${config.httpPort} in ${config.envName} mode...`);
-});
-
-const httpsServerOptions = {
-	key: fs.readFileSync('./https/key.pem'),
-	cert: fs.readFileSync('./https/cert.pem')
-};
-
-const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
-	unifiedServer(req, res);
-});
-
-httpsServer.listen(config.httpsPort, () => {
-	console.log(`HTTPS server listening on port ${config.httpsPort} in ${config.envName} mode...`);
-});
+module.exports = app;
